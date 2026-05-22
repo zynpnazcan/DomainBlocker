0041b4596b775db387ebdcaa2bfac6b8ebe78bc5
zynpnazcan <zeynepnazcan44@gmail.com>
2026-05-22 11:33:50 +0300

    chore: shell route integration, dynamic menus, i18n keys and typescript module declarations added

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,7 +4,10 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [
+  // 1. Ana Sayfa (Mevcut)
   { path: '', component: DashboardComponent, pathMatch: 'full' },
+
+  // 2. Sistem Logları Sayfası (Mevcut - Port 4203)
   {
     path: 'logs', 
     loadComponent: () =>
@@ -15,10 +18,23 @@ const routes: Routes = [
       })
       .then(m => m.LogsComponent) 
       .catch(err => {
-        console.error("Log MFE'sine ulaşılamadı. Sunucu kapalı olabilir:", err);
+        console.error("Log MFE'sine ulaşılamadı:", err);
         throw err; 
       })
-  } 
+  },
+
+  // 3. İstatistikler Sayfası (Port 4204)
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4204/remoteEntry.js',
+        exposedModule: './DashboardModule'
+      })
+      .then(m => m.DashboardModule)
+      .catch(err => console.error("Yüklenemedi:", err))
+  }
 ];
 
 @NgModule({
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
+
   name: 'domain-blocker-shell',
+
   remotes: {
     "mfe1": "http://localhost:4201/remoteEntry.js", 
     "mfe2": "http://localhost:4202/remoteEntry.js", 
     "mfe3": "http://localhost:4203/remoteEntry.js",
+    "mfe3Dashboard": "http://localhost:4204/remoteEntry.js"
   },
+
   shared: {
-    ...shareAll({ singleton: true, strictVersion: false, requiredVersion: 'auto' }),
+    ...shareAll({ 
+      singleton: true, 
+      strictVersion: false, 
+      requiredVersion: 'auto' 
+    }),
+    
     "@ngx-translate/core": { singleton: true, strictVersion: false, requiredVersion: 'auto' },
     "@ngx-translate/http-loader": { singleton: true, strictVersion: false, requiredVersion: 'auto' }
   },
+
 });
+
 module.exports.output = {
   publicPath: "auto",
   scriptType: "text/javascript" 
